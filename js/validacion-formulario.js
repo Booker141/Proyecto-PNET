c060f1013154e1bf2d0a292dadc57cd3460e6e54
CarpioKey <luismi.klavier@gmail.com>
2020-03-17 22:24:13 +0100

    Mejora notable en la validacion

--- js/validacion-formulario.js
+++ js/validacion-formulario.js
@@ -1,20 +1,24 @@
 function validacion(){
 
-    //Valores de los campos Nombre y Apellidos
-    nombre = document.getElementById("Nombre").nodeValue;
-    apellidos = document.getElementById("Apellidos").nodeValue;
+	//Valores de los campos Nombre y Apellidos
+    nombre = document.getElementById("Nombre").value;
+    apellidos = document.getElementById("Apellidos").value;
 
     //Valor del campo Email
 
-    email = document.getElementById("Email").nodeValue;
+    email = document.getElementById("Email").value;
 
     //Valor del campo DNI
 
-    dni = document.getElementById("DNI").nodeValue;
+    dni = document.getElementById("DNI").value;
+	
+	//Identificador de socio
+	
+	socio = document.getElementById("Socio").value;
 
     //Valor respuesta a la pregunta
 
-    respuesta = document.getElementById("Pregunta").nodeValue;
+    respuesta = document.getElementById("Pregunta").value;
 
     //Opciones de la lista de inscripciones
 
@@ -22,107 +26,144 @@ function validacion(){
 
     //Datos para validar fecha nacimiento
    
-    fecha = document.getElementById("FechaNac").nodeValue;
+    fecha = document.getElementById("FechaNac").value;
 	
 	//Fecha de salida
 	
-	salida = document.getElementById("Salida").nodeValue;
+	salida = document.getElementById("Salida").value;
 	
 	//Fecha de regreso
 	
-	vuelta = document.getElementById("Vuelta").nodeValue;
+	vuelta = document.getElementById("Vuelta").value;
+	
+	validado = true;
 
-    if(esAlfabetico(nombre)){
-        alert('El campo Nombre debe contener únicamente caracteres alfabéticos');
-        return false;
+	if(!esAlfabetico(nombre)){
+        alert('El campo Nombre no puede contener números o símbolos.');
+		document.getElementById("Nombre").style.border="1px solid red";
+        validado = false;
 	}
 
-    if(esAlfabetico(apellidos)){
-        alert("El campo Apellidos debe contener únicamente caracteres alfabéticos");
-        return false;
+    if(!esAlfabetico(apellidos)){
+        alert("El campo Apellidos no debe contener números o símbolos.");
+		document.getElementById("Apellidos").style.border="1px solid red";
+        validado = false;
     }
 
-    if(esEmail(email)){
-        alert("El formato de la dirección de correo electrónico es incorrecto");
-        return false;
+    if(!esEmail(email)){
+        alert("El formato de la dirección de correo electrónico es incorrecto (A@B.C).");
+		document.getElementById("Email").style.border="1px solid red";
+        validado = false;
     }
 
-    if(esDNI(dni)){
-        alert("El formato del DNI es incorrecto");
-        return false;
+    if(!esDNI(dni)){
+        alert("El formato del DNI es incorrecto (11111111A).");
+		document.getElementById("DNI").style.border="1px solid red";
+        validado = false;
+    }
+	
+	if(!esSocio(socio)){
+		alert("El identificador de socio proporcionado no es correcto (11111A).");
+		document.getElementById("Socio").style.border="1px solid red";
+		validado = false;
+	}
+	
+	if(!nacimientoOK(fecha)){
+		alert("Debe tener como mínimo 6 años para asistir.");
+		document.getElementById("FechaNac").style.border="1px solid red";
+		validado = false;
+	}
+
+    if(!esCorrecta(respuesta)){
+        alert("La respuesta a la pregunta de confirmación no es correcta (Empieza por R).");
+		document.getElementById("Pregunta").style.border="1px solid red";
+        validado = false;
     }
 
-    if(esCorrecta(respuesta)){
-        alert("La respuesta no es correcta");
-        return false;
-    }
-
-    if(validarFechaNac(fecha)){
-        alert("La fecha debe estar comprendida entre las fechas de la celebración del evento");
-        return false;
-    }
-
-    if(esSeleccionado(indice)){
+    if(!esSeleccionado(indice)){
         alert("Debes seleccionar al menos un tipo de inscripción");
-        return false;
+		document.getElementById("Inscripcion").style.border="1px solid red";
+        validado = false;
     }
-
-
-    return true;
-
+	
+	if(salida != "2020-06-26" && salida != "2020-06-27"){
+		alert("Se ha introducido una fecha de salida que está fuera del evento.");
+		alert(salida);
+		document.getElementById("Salida").style.border="1px solid red";
+		validado = false;
+	}
+	
+	if(vuelta != "2020-06-26" && vuelta != "2020-06-27"){
+		alert("Se ha introducido una fecha de regreso que está fuera del evento.");
+		document.getElementById("Vuelta").style.border="1px solid red";
+		validado = false;
+	}
+	
+	if(vuelta < salida){
+		alert("Ha introducido un viaje de regreso anterior al de ida.");
+		document.getElementById("Salida").style.border="1px solid red";
+		document.getElementById("Vuelta").style.border="1px solid red";
+		validado = false;
+	}
+	
+	return validado;
 }
 
 /**
- * Método esAlfabetico(nodeValue valor)
- * @param {*} nodeValue 
+ * Método esAlfabetico(value valor)
+ * @param {*} value 
  * @param {*} valor 
  * Comprueba que los campos que sean necesario solo contienen caracteres alfabéticos
  */
 
-function esAlfabetico(nodeValue valor){
-
-    //Variable que contiene las posibles letras del alfabeto
-    var letras = /^[A-Za-z]+$/;
-
-    //Comprueba si el valor recibido concuerda
-    if(!(valor.value.match(letras))){
+function esAlfabetico(valor){
+    var letras = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/;
+    if(!(valor.match(letras))){
         return false;
-    }
+    } else return true;
 }
 /**
- * Método esEmail(nodeValue email)
- * @param {*} nodeValue 
+ * Método esEmail(value email)
+ * @param {*} value 
  * @param {*} email
  * Comprueba que el formato de la dirección de correo electrónico sea correcto 
  */
 
-function esEmail(nodeValue email){
-
-    if(!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email))){
+function esEmail(email){
+	var regexp = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    if(!(regexp.test(email))){
         return false;
-    }
-    
+    } else return true;
 }
 
 /**
- * Método esDNI(nodeValue dni)
- * @param {*} nodeValue 
+ * Método esDNI(value dni)
+ * @param {*} value 
  * @param {*} dni 
  * Comprueba que el formato del DNI sea correcto
  */
 
-function esDNI(nodeValue dni){
-
+function esDNI(dni){
     var letrasDNI = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
-
-    if( !(/^\d{8}[A-Z]$/.dni(valor)) ) {
+    if(!(/^\d{8}[A-Z]$/.test(dni))) {
         return false;
     }
-
     if(!(dni.charAt(8) == letrasDNI[(dni.substring(0, 8))%23])) {
         return false;
     }
+	return true;
+}
 
+function esSocio(socio){
+	var regexp = /^[0-9]{5,5}[A-Z]+$/;
+	if(!regexp.test(socio)) return false;
+	else return true;
+}
+
+function nacimientoOK(fecha){
+	var maximo = new Date(2015, 1, 1);
+	if(fecha > maximo) return false;
+	else return true;
 }
 
 /**
@@ -131,41 +172,21 @@ function esDNI(nodeValue dni){
  * @param {*} indice 
  * Comprueba que haya una opción seleccionada de la lista
  */
-function esSeleccionado(long indice){
-
+function esSeleccionado(indice){
     if((indice == null || indice == 0)){
         return false;
-    }
-
+    } else return true;
 }
 
 /**
- * Método validarFecha(Date fecha)
- *  @param {*} Date
- *  @param {*} fecha 
- * Comprueba que las fechas están comprendidas entre las fechas de la celebración del evento
- */
-
-function validarFechaNac(Date fecha){
-
-    if(!(isNaN(fecha))){
-        return false;
-    }
-
-}
-
-/**
- * Método esCorrecta(nodeValue respuesta)
- * @param {*} nodeValue 
+ * Método esCorrecta(value respuesta)
+ * @param {*} value 
  * @param {*} respuesta
  * Comprueba que la respuesta a la pregunta es correcta
  */
 
-function esCorrecta(nodeValue respuesta){
-
+function esCorrecta(respuesta){
     if(!(respuesta == "Rossini" || respuesta == "rossini")){
         return false;
-    }
-
+    } else return true;
 }
-
